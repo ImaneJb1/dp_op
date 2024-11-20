61fef01b0abcb5ddd0a5130a06d0a70e62bf3be7
Imane Joubair <ijoubair@c3r6p2.1337.ma>
2024-11-20 12:17:16 +0100

    [ADD] variadic function sum_then_all

---
+++ variadic_functions/sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...)
+{
+    unsigned int i;
+    int result;
+    va_list nums;
+  
+    result = 0;
+    i = 0;
+    if(n <= 0)
+        return(0);
+   
+    va_start(nums, n);
+    while(i < n)
+    {
+        result += va_arg(nums, int); 
+        i++; 
+        // printf("result%d= %d\n", i, result);
+    }
+    return (result);
+}
+
+int main()
+{
+     unsigned int n;
+    scanf("%d", &n);
+    printf("result = %d\n", sum_them_all(n, 5, 4, 3, 8));
+}
